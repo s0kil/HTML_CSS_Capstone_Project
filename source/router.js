ed1f3c4bf3c4962a601e27e1ca471d95715e565e
Daniel Sokil <dtechtours@gmail.com>
2020-05-22 14:23:18 -0400

    Initial Project Scafold

---
+++ source/router.js
@@ -0,0 +1,32 @@
+(async () => {
+  const APPLICATION_ROUTES = {
+    "/": {
+      html: "./pages/home.html",
+    },
+    "404": {
+      html: "./pages/404.html",
+    },
+  };
+
+  const requestedPath = window.location.pathname;
+  const route = APPLICATION_ROUTES[requestedPath] || APPLICATION_ROUTES["404"];
+  const routeHtml = await fetch(route.html).then((r) => r.text());
+
+  const renderRouteHtml = (html) => {
+    const mainContent = document.getElementById("main-content");
+    mainContent.innerHTML = html;
+  };
+
+  const loadRouteScript = (route) =>
+    route.script ? window.loadjs(route.script) : undefined;
+
+  if (/complete|interactive|loaded/.test(document.readyState)) {
+    renderRouteHtml(routeHtml);
+    loadRouteScript(route);
+  } else {
+    window.addEventListener("DOMContentLoaded", () => {
+      renderRouteHtml(routeHtml);
+      loadRouteScript(route);
+    });
+  }
+})();
